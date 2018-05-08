f09aa5323a08d5d680a0a4473cbdde3f0667edce
Amulya Reddy <bamulyareddy@gmail.com>
2018-05-07 23:26:07 -0500

    'ComponentsAndProps'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
- setInterval(function(){
+function Cartoon(props){
+  return <h1>Hello, {props.name} on {props.show}</h1>
+}
+
+function Show(){
+  return <div>
+<Cartoon name="pikachu" show="pokeman"/>
+<Cartoon name="Barbie" show="DreamHouse"/>
+  </div>
+}
+// class Cartoon extends React.Component{
+//   render(){
+//     return <h1>My name is {this.props.name}</h1>
+//
+//   }
+// }
+
    ReactDOM.render(
-     <h1>Time Now is:---{new Date().toLocaleTimeString()}</h1>,
+     <Show/> ,
      document.getElementById('root')
    );
-},2000);
